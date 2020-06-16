3f9ef323cae312a7c9a601b34a5872a24bbfcacc
bloodysysy73 <sylvain.guehria@gmail.com>
2020-06-16 21:06:47 +0200

    ajout mapping formulaire

--- src/variables/specMachineVariable.js
+++ src/variables/specMachineVariable.js
@@ -1,10 +1,52 @@
 export const machines = [
     '6MCR',
-    'Ukraine',
-    'United Kingdom',
-    'United States',
-    'Vatican City'
+    '8MCR',
+    '10MCR',
+    '10MCR Tier4',
+    '7MWR',
+    '9MWR',
+    '11MWR',
+    '15MWR',
+    '10MSX',
+    '12MSX',
+    '14MBX',
+    '14MXT',
+    '12MXT',
+    '12MTX',
+    '12MXT Speeder',
+    '714MWe',
+    '714MCe',
+    '714Mw',
+    '712MC',
+    '714MC',
+    '12MX'
+
 ];
+
+export const machinesSpec = [
+    {
+        id: '1',
+        name: '6MCR',
+        numSerie: ['120015', '120100']
+    },
+    {
+        id: '2',
+        name: '8MCR',
+        numSerie: ['130040', '130900 + RR']
+    },
+    {
+        id: '3',
+        name: '10MCR',
+        numSerie: ['140033']
+    },
+    {
+        id: '4',
+        name: '10MCR Tier4',
+        numSerie: ['140300']
+    },
+
+];
+
 
 export const nSerie = [
     '120100',
@@ -16,18 +58,7 @@ export const nSerie = [
 
 export const clim = [
     'oui',
-    'Ukraine',
-    'United Kingdom',
-    'United States',
-    'Vatican City'
-];
-
-export const deplacement = [
-    'xxx',
-    'yyy',
-    'United Kingdom',
-    'United States',
-    'Vatican City'
+    'non',
 ];
 
 export const entretien250 = [
