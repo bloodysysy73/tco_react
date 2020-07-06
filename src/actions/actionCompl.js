94082b5dc8be788098c5b5f35dcabbfa9b328b46
bloodysysy73 <sylvain.guehria@gmail.com>
2020-07-06 20:12:42 +0200

    ajout ref au store

--- src/actions/actionCompl.js
+++ src/actions/actionCompl.js
@@ -1,9 +1,16 @@
-import { DEFINETIME } from 'actions';
+import { DEFINETIME, DEFINEREF } from 'actions';
 
 
 export const defineTime = (name, value) => {
     return {
         type: DEFINETIME,
         payload: { name, value }
+    };
+};
+
+export const defineRef = (value) => {
+    return {
+        type: DEFINEREF,
+        payload: { value }
     };
 };
