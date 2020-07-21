7a4ca6c853c181f1eaa306a13997f28237d1603b
bloodysysy73 <sylvain.guehria@gmail.com>
2020-07-21 20:54:04 +0200

    incrementation Autres Services Ajoutés

--- src/actions/actionServiceajoutes.js
+++ src/actions/actionServiceajoutes.js
@@ -1,10 +1,18 @@
-import { SETSERVICENUMBER } from 'actions';
+import { SETSERVICENUMBER, UPDATE_ATTRIBUT } from 'actions';
 
 
 export const setServiceNumber = (value) => {
+
     return {
         type: SETSERVICENUMBER,
         payload: value
+    };
+};
+
+export const definiAttributline = (name, value) => {
+    return {
+        type: UPDATE_ATTRIBUT,
+        payload: { name, value }
     };
 };
 
